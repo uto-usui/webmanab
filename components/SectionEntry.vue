1f9e40a02da128f28f2cfa5e5dcde538832ad7ac
uto-usui <me@uto-usui.com>
2019-02-13 23:27:18 +0900

    ✨ add component SectionEntry

---
+++ components/SectionEntry.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="entry">
+    <slot />
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'SectionList'
+}
+</script>
+
+<style lang="scss" scoped>
+.entry {
+  max-width: 940px;
+  margin-right: auto;
+  margin-left: auto;
+  //
+  @include desktop {
+    padding-right: 30px;
+    padding-left: 30px;
+  }
+  //
+  @include desktop {
+    padding-right: 40px;
+    padding-left: 40px;
+  }
+}
+</style>
